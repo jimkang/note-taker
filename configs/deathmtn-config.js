6a9a46be7cf7eddd60744e971a256ae9d73e6dda
Jim Kang <jimkang@gmail.com>
2020-01-01 15:28:36 -0500

    Reorder footer, clean out dormant stuff, add search link.

--- configs/deathmtn-config.js
+++ configs/deathmtn-config.js
@@ -10,17 +10,7 @@ module.exports = {
     fileAbstractionType: 'LocalGit',
     footerHTML: `<footer id="footer">
     <div>
-      Good reads on clean web pages:
-      <ul>
-      <li><a href="http://blog.beaugunderson.com/">Beau's thoughts</a></li>
-      <li><a href="http://plastic-idolatry.com/erik/timeline.html">Erik's timeline</a></li>
-      <li><a href="https://avoision.com/blog/">Avoision</a></li>
-      <li><a href="https://memex.naughtons.org">John Naughton's online diary</a></li>
-      <li><a href='https://webring.xxiivv.com/#random' target='_blank'>A web ring! <img src='https://webring.xxiivv.com/icon.black.svg' style="width: 64px;"></a></li>
-      </ul>
-    </div>
-    <div>
-    <div>This site is updated via <a href="https://github.com/jimkang/note-taker">note-taker</a> and <a href="https://github.com/jimkang/note-sender">note-sender</a>. note-taker is an adapter for <a href="https://github.com/jimkang/static-web-archive">static-web-archive</a>.
+      <a href="https://smidgeo.com/notes/deathmtn/search>Search this weblog</a>
     </div>
     <div>
       <a href="https://smidgeo.com/notes/deathmtn/rss/index.rss">RSS feed</a>
@@ -33,11 +23,22 @@ module.exports = {
     </div>
     <br /><br />
     <div>
+    <div>This site is updated via <a href="https://github.com/jimkang/note-taker">note-taker</a> and <a href="https://github.com/jimkang/note-sender">note-sender</a>. note-taker is an adapter for <a href="https://github.com/jimkang/static-web-archive">static-web-archive</a>.
+    </div>
+    <div>
       Want to read more stuff?
+      <div>
+        Good reads on clean web pages:
+        <ul>
+        <li><a href="https://avoision.com/blog/">Avoision</a></li>
+        <li><a href="https://memex.naughtons.org">John Naughton's online diary</a></li>
+        </ul>
+      </div>
       <ul>
         <li><a href="https://smidgeo.com/bots/">My bots</a></li>
         <li><a href="https://tinyletter.com/jimkang">My newsletter about my projects and plans</a></li>
         <li><a href="https://jimkang.com">My personal web site, whatever that means</a></li>
+      </ul>
     </footer>
     `,
     generateRSS: true,
